9bbbe38942f0006a9d88bdbce6bd09444d6e6743
OpusMagnus5 <damianbodzioch95@gmail.com>
2024-03-13 18:35:45 +0100

    Add Gateway service

---
+++ Gateway/src/main/java/pl/bodzioch/damian/GatewayApp.java
@@ -0,0 +1,14 @@
+package pl.bodzioch.damian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+class GatewayApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApp.class, args);
+    }
+}
